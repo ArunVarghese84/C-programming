cd36a80df32eb2fdedc49b9e16cdbd645514de6f
Arun Varghese <arunvarghese084@gmail.com>
2022-11-09 19:03:56 +0530

    Update A14_getnbits.c

--- Emertxe/A14_getnbits.c
+++ Emertxe/A14_getnbits.c
@@ -1,6 +1,6 @@
 /*
 Name: Arun Varghese
-Date: 2
+Date: 2/11/2022
 Description:Program to get 'n' bits of a given number 
 Sample Input:Enter the number: 10
 
@@ -9,23 +9,23 @@ Sample Output:Result = 2
 */
 
 #include<stdio.h>
-int getnbits(int,int);
+int getnbits(int,int);  //function declaration
 int main()
 {
-    int num,bit,res=0;
+    int num,bit,res=0;    //variables in main function
     printf("Enter the number:");
-    scanf("%d", &num);
+    scanf("%d", &num);     //number is read
     printf("Enter number of bits:");
-    scanf("%d", &bit);
+    scanf("%d", &bit);     //no. of bits is read
 
-    res=getnbits(num,bit);
-    printf("Result = %d\n", res);
+    res=getnbits(num,bit);          //function call and the value returned is collected in res
+    printf("Result = %d\n", res);      // result is printed
 }
 
-int getnbits(int a, int b)
+int getnbits(int a, int b)    //function definition
 {
-    int mask,result;
-    mask=((1<<b)-1);
-    result= a & mask;
-    return result;
+    int mask,result;     //variables for the function
+    mask=((1<<b)-1);     //mask created
+    result= a & mask;     //result is found
+    return result;       //result is retuned to function call
 }
