2957c3a0c1df60a1b5804368635fd8dd389016d5
Arun Varghese <arunvarghese084@gmail.com>
2022-11-09 19:10:52 +0530

    Update and rename A23_(isxdigit().c to A23_isxdigit().c

--- Emertxe/A23_isxdigit().c
+++ Emertxe/A23_isxdigit().c
@@ -8,18 +8,18 @@ Sample Output:Entered character is an hexadecimal digit
 
 #include <stdio.h>
 
-int is_xdigit(int);
+int is_xdigit(char);  //function declaration
 
 int main()
 {
     char ch;
     short ret=0;
 
-    printf("Enter a character: ");
+    printf("Enter a character: ");     //character is read
     scanf("%c", &ch);
 
-    ret = is_xdigit(ch);
-    if(ret==1)
+    ret = is_xdigit(ch);        //function call
+    if(ret==1)                 //o/p with respect to the value returned from function
     {
 	printf("Entered character is an hexadecimal digit\n");
     }
@@ -31,11 +31,11 @@ int main()
     return 0;
 }
 
-int is_xdigit(int ch)
+int is_xdigit(char ch)       //function defenition
 {
     int ret=0;
 
-    if( (ch>='0' && ch<='9') || (ch>='a' && ch<='f') || (ch>='A' && ch<='F'))
+    if( (ch>='0' && ch<='9') || (ch>='a' && ch<='f') || (ch>='A' && ch<='F'))     //checking whether a hexadecimal value and return 1 if true
     {
 	return 1;
     }
