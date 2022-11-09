21e9a970b011a67715858428578fc8512ca8ccea
Arun Varghese <arunvarghese084@gmail.com>
2022-11-09 19:08:51 +0530

    Update A20_CircularLeftShift.c

--- Emertxe/A20_CircularLeftShift.c
+++ Emertxe/A20_CircularLeftShift.c
@@ -10,12 +10,12 @@ Sample Output:Result in Binary: 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 
 #include <stdio.h>
 
-int circular_left(int, int);
+int circular_left(int, int);      //functions declaration
 int print_bits(int);
 
 int main()
 {
-    int num, n, ret;
+    int num, n, ret;       //variable declaration
 
     printf("Enter the num:");
     scanf("%d", &num);
@@ -23,28 +23,28 @@ int main()
     printf("Enter n:");
     scanf("%d", &n);
 
-    ret = circular_left(num, n);
+    ret = circular_left(num, n);       //function for circular left shifting is called and value is collected in ret
 
-    print_bits(ret);
+    print_bits(ret);            //function for printing result in binary is called
 }
 
-int circular_left(int num, int n)
+int circular_left(int num, int n)          //function defenition for function for circular left shifting
 {
-    int temp1,temp2;
+    int temp1,temp2; 
     if(n<=32)
     {
-	temp1=num>>(32-n) & ((1<<n)-1);
-	temp2=num<<n;
-	num=temp1|temp2;
+	temp1=num>>(32-n) & ((1<<n)-1);        //n bits in MSB is retrived
+	temp2=num<<n;                         //number is left shifted n times
+	num=temp1|temp2;                      //retrived MSB bit is added to LSB
     }
-    return num;
+    return num;                        //result is returned
 }
 
-int print_bits(int ret)
+int print_bits(int ret)             //function to print the result in binary
 {
     int i,x;
-    printf("Result in Binary:");
-    for(i=32;i>=0;i--)
+    printf("Result in Binary: ");
+    for(i=31;i>=0;i--)
     {
 	x=ret &(1<<i);
 	if(x==0)
@@ -57,4 +57,3 @@ int print_bits(int ret)
 	}
     }
 }
-
