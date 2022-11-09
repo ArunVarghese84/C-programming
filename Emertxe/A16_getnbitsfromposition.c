4c05e1849e052a585176de7f0f2447a94fcbcab4
Arun Varghese <arunvarghese084@gmail.com>
2022-11-09 19:04:51 +0530

    Update A16_getnbitsfromposition.c

--- Emertxe/A16_getnbitsfromposition.c
+++ Emertxe/A16_getnbitsfromposition.c
@@ -12,21 +12,21 @@ Sample Output:Result = 3
 
 #include <stdio.h>
 
-int get_nbits_from_pos(int, int, int);
+int get_nbits_from_pos(int, int, int);  //function declaration
 
 int main()
 {
-    int num, n, pos, res = 0;
+    int num, n, pos, res = 0;   //variable declaration
     
     printf("Enter num, n and val:");
     scanf("%d%d%d", &num, &n, &pos);
     
-    res = get_nbits_from_pos(num, n, pos);
+    res = get_nbits_from_pos(num, n, pos);   //function call
     
     printf("Result = %d\n", res);
 }
 
-int get_nbits_from_pos(int num,int n,int pos)
+int get_nbits_from_pos(int num,int n,int pos)     //function defenition
 {
-    return(((num>>((pos+1)-n))) & ((1<<n)-1));
+    return(((num>>((pos+1)-n))) & ((1<<n)-1));     //creating leftshift and doing bitwise and with mask to get result
 }
